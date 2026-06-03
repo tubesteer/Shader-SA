9511ee5a6a82b1cf1333f9315d32d20f24295f7c
tubesteer <praditdimas1@gmail.com>
2026-06-04 03:16:35 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,10 +1,22 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := modtemplate
-LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp
-LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
-LOCAL_C_INCLUDES += ./include
-LOCAL_LDLIBS += -llog
+
+# Nama plugin .so yang akan dihasilkan (libGTASA_ShaderLoader30.so)
+LOCAL_MODULE    := GTASA_ShaderLoader30
+
+# Daftarkan semua file C++ yang akan di-compile
+# main.cpp di root, dan texture_loader.cpp di dalam folder mod
+LOCAL_SRC_FILES := main.cpp \
+                   mod/texture_loader.cpp
+
+# Beritahu compiler untuk mencari file header (.h) di dalam folder mod
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/mod
+
+# Hubungkan library bawaan Android: Log sistem, Android EGL, dan OpenGL ES 3
+LOCAL_LDLIBS    := -llog -lEGL -lGLESv3
+
+# Flag tambahan: Menggunakan standar C++17 dan optimasi performa tingkat tinggi (O3)
+LOCAL_CFLAGS    := -O3 -std=c++17
+
 include $(BUILD_SHARED_LIBRARY)
