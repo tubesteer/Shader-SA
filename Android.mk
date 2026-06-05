fb7317aa25386f4b928407ab3314feba7fbe8eec
tubesteer <praditdimas1@gmail.com>
2026-06-05 18:15:11 +0700

    Update: Add shader_manager and hook_manager to build, optimize for both 32/64-bit

--- Android.mk
+++ Android.mk
@@ -6,19 +6,26 @@ LOCAL_CPP_EXTENSION := .cpp .cc
 # Nama file .so yang akan dihasilkan nanti
 LOCAL_MODULE    := GTASA_ShaderLoader30
 
-# Daftarkan file bawaan template DAN file kustom kita (texture_loader.cpp)
+# Daftarkan semua file source termasuk shader & hook manager
 LOCAL_SRC_FILES := main.cpp \
                    mod/logger.cpp \
                    mod/config.cpp \
-                   mod/texture_loader.cpp
+                   mod/texture_loader.cpp \
+                   mod/shader_manager.cpp \
+                   mod/hook_manager.cpp
 
-# Flag optimasi bawaan template, menggunakan standar C++17
-LOCAL_CFLAGS += -O3 -mfloat-abi=softfp -DNDEBUG -std=c++17
+# Flag optimasi dengan C++17 support
+LOCAL_CFLAGS += -O3 -DNDEBUG -std=c++17
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+    LOCAL_CFLAGS += -fPIC -march=armv8-a
+else
+    LOCAL_CFLAGS += -mfloat-abi=softfp
+endif
 
-# Tetap gunakan folder include bawaan template untuk mencari header amlmod.h dkk
+# Tetap gunakan folder include
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 
-# WAJIB TAMBAH: Hubungkan library grafis EGL dan OpenGL ES 3 (-lEGL -lGLESv3)
+# Link dengan library grafis EGL dan OpenGL ES 3
 LOCAL_LDLIBS += -llog -lEGL -lGLESv3
 
 include $(BUILD_SHARED_LIBRARY)
