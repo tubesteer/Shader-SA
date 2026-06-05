381ab6e010e169f71df1727748076e045046ffd2
tubesteer <praditdimas1@gmail.com>
2026-06-05 18:21:40 +0700

    Fix: Add -fexceptions flag for exception handling support

--- Android.mk
+++ Android.mk
@@ -14,8 +14,8 @@ LOCAL_SRC_FILES := main.cpp \
                    mod/shader_manager.cpp \
                    mod/hook_manager.cpp
 
-# Flag optimasi dengan C++17 support
-LOCAL_CFLAGS += -O3 -DNDEBUG -std=c++17
+# Flag optimasi dengan C++17 support + exception handling
+LOCAL_CFLAGS += -O3 -DNDEBUG -std=c++17 -fexceptions
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
     LOCAL_CFLAGS += -fPIC -march=armv8-a
 else
