8a0f27342630877f5d3491806ad6d6ae4453398d
tubesteer <praditdimas1@gmail.com>
2026-06-05 18:03:34 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,64 +2,87 @@
 #include <mod/logger.h>
 #include <string>
 #include <fstream>
-#include <GLES2/gl2.h>
+#include <streambuf>
 
-int (*ES2Shader_Build)(void* thiz, const char* vertexSource, const char* fragmentSource);
-int (*ES2Shader_InitializeAfterCompile)(void* thiz);
+MYMOD(com.example.shaderloader, ES2ShaderLoader, 1.2, YourName)
 
-float g_ShaderTime = 0.0f;
+int (*ES2Shader_Build_orig)(void* _this, const char* pixelSource, const char* vertexSource);
 
-std::string LoadShader(const std::string& fileName) {
-    std::string path = "/sdcard/Android/data/com.rockstargames.gtasa/files/shaders/" + fileName;
+std::string ReadShaderFile(const std::string& filename) {
+    std::string path = aml->GetAndroidDataPath() + "/shaders/" + filename;
     std::ifstream file(path);
     if (!file.is_open()) return "";
     return std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 }
 
-int __fastcall Hook_ES2Shader_Build(void* thiz, const char* vertexSource, const char* fragmentSource) {
-    std::string vSource(vertexSource);
-    std::string fSource(fragmentSource);
-    std::string finalVertex = vSource;
-    std::string finalFragment = fSource;
-
-    if (vSource.find("SURF_TEX") != std::string::npos || vSource.find("vVehicleColor") != std::string::npos) {
-        std::string customV = LoadShader("vehicle_vertex.glsl");
-        std::string customF = LoadShader("vehicle_fragment.glsl");
-        if (!customV.empty()) finalVertex = customV;
-        if (!customF.empty()) finalFragment = customF;
-    }
-    else if (vSource.find("Water") != std::string::npos || fSource.find("u_waterColor") != std::string::npos) {
-        std::string customV = LoadShader("water_vertex.glsl");
-        std::string customF = LoadShader("water_fragment.glsl");
-        if (!customV.empty()) finalVertex = customV;
-        if (!customF.empty()) finalFragment = customF;
-    }
-
-    return ES2Shader_Build(thiz, finalVertex.c_str(), finalFragment.c_str());
+unsigned int GetShaderHash(const char* str) {
+    unsigned int hash = 5381;
+    int c;
+    while ((c = *str++)) hash = ((hash << 5) + hash) + c;
+    return hash;
 }
 
-int __fastcall Hook_ES2Shader_InitializeAfterCompile(void* thiz) {
-    int result = ES2Shader_InitializeAfterCompile(thiz);
-    unsigned int glProgram = *(unsigned int*)((uintptr_t)thiz + (250 * sizeof(int)));
-    int uTimeLocation = glGetUniformLocation(glProgram, "u_CustomTime");
-    if (uTimeLocation != -1) {
-        glUniform1f(uTimeLocation, g_ShaderTime);
+std::string PrepareShaderSource(const std::string& customSource, bool isVertex) {
+    if (customSource.empty()) return "";
+    std::string processed = customSource;
+
+    if (processed.find("#version 300 es") != std::string::npos) {
+        if (processed.find("precision") == std::string::npos && !isVertex) {
+            size_t pos = processed.find("#version 300 es");
+            processed.insert(pos + 16, "\nprecision highp float;\n");
+        }
     }
-    return result;
+    return processed;
 }
 
-MYMOD(com.modder.gta_shader_loader, GTA SA Shader Loader, 1.0, ModderName)
-BEGIN_DEMOD
-    void* hGTASA = aml->GetLib("libgtasa.so");
-    if (hGTASA) {
-        uintptr_t fn_Build = aml->GetSym(hGTASA, "_ZN9ES2Shader5BuildEPKcS1_");
-        uintptr_t fn_InitAfter = aml->GetSym(hGTASA, "_ZN9ES2Shader24InitializeAfterCompileEv");
+int ES2Shader_Build_hook(void* _this, const char* pixelSource, const char* vertexSource) {
+    unsigned int pHash = GetShaderHash(pixelSource);
+    unsigned int vHash = GetShaderHash(vertexSource);
+
+    std::string rawPixel = ReadShaderFile("P_" + std::to_string(pHash) + ".glsl");
+    std::string rawVertex = ReadShaderFile("V_" + std::to_string(vHash) + ".glsl");
+
+    std::string finalPixel = rawPixel.empty() ? pixelSource : PrepareShaderSource(rawPixel, false);
+    std::string finalVertex = rawVertex.empty() ? vertexSource : PrepareShaderSource(rawVertex, true);
+
+    if (!rawPixel.empty()) LOGI("Pixel Shader [%X] diganti ke file eksternal (GLSL v3).", pHash);
+    if (!rawVertex.empty()) LOGI("Vertex Shader [%X] diganti ke file eksternal (GLSL v3).", vHash);
+
+    return ES2Shader_Build_orig(_this, finalPixel.c_str(), finalVertex.c_str());
+}
+
+extern "C" void OnModLoad() {
+    logger->SetTag("ShaderLoader");
+    void* libGame = aml->GetLibHandle("libgta.so");
+    
+    if (libGame) {
+        uintptr_t libBase = aml->GetLib(libGame);
+
+        uintptr_t buildOffset = 0x1ccc04; 
+        aml->Hook((void*)(libBase + buildOffset), (void*)ES2Shader_Build_hook, (void**)&ES2Shader_Build_orig);
+        LOGI("Hook pada ES2Shader::Build berhasil!");
+
+        uintptr_t initGraphicsAddr = libBase + 0x2690b4;
+        bool patchSuccess = false;
         
-        if (fn_Build) {
-            HOOKB(fn_Build, Hook_ES2Shader_Build, &ES2Shader_Build);
+        for (uintptr_t i = 0; i < 100; i += 2) {
+            unsigned short* currentIns = (unsigned short*)(initGraphicsAddr + i);
+            
+            if (*currentIns == 0x2302) {
+                unsigned char gles3_value = 0x03;
+                aml->Write((void*)(initGraphicsAddr + i), (uintptr_t)&gles3_value, 1);
+                LOGI("Berhasil menemukan instruksi EGL Context! Diubah ke OpenGL ES 3.0 pada offset: 0x%X", 0x2690b4 + i);
+                patchSuccess = true;
+                break;
+            }
         }
-        if (fn_InitAfter) {
-            HOOKB(fn_InitAfter, Hook_ES2Shader_InitializeAfterCompile, &ES2Shader_InitializeAfterCompile);
+
+        if (!patchSuccess) {
+            LOGW("Peringatan: Instruksi 'MOVS R3, #2' tidak ditemukan secara otomatis di fungsi initGraphics.");
         }
+
+        LOGI("Shader Loader & GLES3 Forcer sukses diinisialisasi!");
+    } else {
+        LOGE("Gagal memuat atau menemukan handle libgta.so!");
     }
-END_DEMOD
+}
