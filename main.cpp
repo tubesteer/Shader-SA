1d22f2a5dc562930727ad479cb48966f7d17d78b
tubesteer <praditdimas1@gmail.com>
2026-06-05 18:06:30 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ MYMOD(com.example.shaderloader, ES2ShaderLoader, 1.2, YourName)
 int (*ES2Shader_Build_orig)(void* _this, const char* pixelSource, const char* vertexSource);
 
 std::string ReadShaderFile(const std::string& filename) {
-    std::string path = aml->GetAndroidDataPath() + "/shaders/" + filename;
+    std::string path = std::string(aml->GetAndroidDataPath()) + "/shaders/" + filename;
     std::ifstream file(path);
     if (!file.is_open()) return "";
     return std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
@@ -45,8 +45,8 @@ int ES2Shader_Build_hook(void* _this, const char* pixelSource, const char* verte
     std::string finalPixel = rawPixel.empty() ? pixelSource : PrepareShaderSource(rawPixel, false);
     std::string finalVertex = rawVertex.empty() ? vertexSource : PrepareShaderSource(rawVertex, true);
 
-    if (!rawPixel.empty()) LOGI("Pixel Shader [%X] diganti ke file eksternal (GLSL v3).", pHash);
-    if (!rawVertex.empty()) LOGI("Vertex Shader [%X] diganti ke file eksternal (GLSL v3).", vHash);
+    if (!rawPixel.empty()) logger->Info("Pixel Shader [%X] diganti ke file eksternal (GLSL v3).", pHash);
+    if (!rawVertex.empty()) logger->Info("Vertex Shader [%X] diganti ke file eksternal (GLSL v3).", vHash);
 
     return ES2Shader_Build_orig(_this, finalPixel.c_str(), finalVertex.c_str());
 }
@@ -56,11 +56,11 @@ extern "C" void OnModLoad() {
     void* libGame = aml->GetLibHandle("libgta.so");
     
     if (libGame) {
-        uintptr_t libBase = aml->GetLib(libGame);
+        uintptr_t libBase = aml->GetLib("libgta.so");
 
         uintptr_t buildOffset = 0x1ccc04; 
         aml->Hook((void*)(libBase + buildOffset), (void*)ES2Shader_Build_hook, (void**)&ES2Shader_Build_orig);
-        LOGI("Hook pada ES2Shader::Build berhasil!");
+        logger->Info("Hook pada ES2Shader::Build berhasil!");
 
         uintptr_t initGraphicsAddr = libBase + 0x2690b4;
         bool patchSuccess = false;
@@ -70,19 +70,19 @@ extern "C" void OnModLoad() {
             
             if (*currentIns == 0x2302) {
                 unsigned char gles3_value = 0x03;
-                aml->Write((void*)(initGraphicsAddr + i), (uintptr_t)&gles3_value, 1);
-                LOGI("Berhasil menemukan instruksi EGL Context! Diubah ke OpenGL ES 3.0 pada offset: 0x%X", 0x2690b4 + i);
+                aml->Write((uintptr_t)(initGraphicsAddr + i), (uintptr_t)&gles3_value, 1);
+                logger->Info("Berhasil menemukan instruksi EGL Context! Diubah ke OpenGL ES 3.0 pada offset: 0x%X", 0x2690b4 + i);
                 patchSuccess = true;
                 break;
             }
         }
 
         if (!patchSuccess) {
-            LOGW("Peringatan: Instruksi 'MOVS R3, #2' tidak ditemukan secara otomatis di fungsi initGraphics.");
+            logger->Warn("Peringatan: Instruksi 'MOVS R3, #2' tidak ditemukan secara otomatis di fungsi initGraphics.");
         }
 
-        LOGI("Shader Loader & GLES3 Forcer sukses diinisialisasi!");
+        logger->Info("Shader Loader & GLES3 Forcer sukses diinisialisasi!");
     } else {
-        LOGE("Gagal memuat atau menemukan handle libgta.so!");
+        logger->Error("Gagal memuat atau menemukan handle libgta.so!");
     }
 }
